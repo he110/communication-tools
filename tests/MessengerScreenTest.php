c101b5364f2d4120dc07bef7ea1f1d957470b179
Ilya S. Zobenko <i.zobenko@ecomilk.ru>
2019-08-10 17:46:31 +0300

    Unit tests for screen button methods

--- tests/MessengerScreenTest.php
+++ tests/MessengerScreenTest.php
@@ -9,6 +9,7 @@
 namespace He110\CommunicationToolsTests;
 
 use He110\CommunicationTools\MessengerScreen;
+use He110\CommunicationTools\ScreenItems\Button;
 use PHPUnit\Framework\TestCase;
 
 class MessengerScreenTest extends TestCase
@@ -42,9 +43,20 @@ class MessengerScreenTest extends TestCase
 
     }
 
+    /**
+     * @covers \He110\CommunicationTools\MessengerScreen::addButtonText()
+     * @covers \He110\CommunicationTools\MessengerScreen::getContent()
+     */
     public function testAddButtonLink()
     {
-
+        $url = "https://ya.ru";
+        $this->assertEmpty($this->screen->getContent());
+        $this->screen->addButtonLink(__METHOD__, $url);
+        $this->assertCount(1, $this->screen->getContent());
+        list($item) = $this->screen->getContent();
+        $this->assertEquals(__METHOD__, $item["label"]);
+        $this->assertEquals(Button::BUTTON_TYPE_URL, $item["type"]);
+        $this->assertEquals($url, $item["url"]);
     }
 
     public function testAddVoice()
@@ -52,14 +64,33 @@ class MessengerScreenTest extends TestCase
 
     }
 
+    /**
+     * @covers \He110\CommunicationTools\MessengerScreen::addButtonText()
+     * @covers \He110\CommunicationTools\MessengerScreen::getContent()
+     */
     public function testAddButtonText()
     {
-
+        $this->assertEmpty($this->screen->getContent());
+        $this->screen->addButtonText(__METHOD__);
+        $this->assertCount(1, $this->screen->getContent());
+        list($item) = $this->screen->getContent();
+        $this->assertEquals(__METHOD__, $item["label"]);
+        $this->assertEquals(Button::BUTTON_TYPE_TEXT, $item["type"]);
     }
 
+    /**
+     * @covers \He110\CommunicationTools\MessengerScreen::addButtonCallback()
+     * @covers \He110\CommunicationTools\MessengerScreen::getContent()
+     */
     public function testAddButtonCallback()
     {
-
+        $this->assertEmpty($this->screen->getContent());
+        $this->screen->addButtonCallback(__METHOD__, function ($a) { echo __METHOD__; });
+        $this->assertCount(1, $this->screen->getContent());
+        list($item) = $this->screen->getContent();
+        $this->assertEquals(__METHOD__, $item["label"]);
+        $this->assertEquals(Button::BUTTON_TYPE_CALLBACK, $item["type"]);
+        $this->assertIsCallable($item["content"]);
     }
 
     public function setUp(): void
