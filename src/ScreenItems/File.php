55707881c2fdb7e324cd0477cb4c31188c4d1b47
Ilya S. Zobenko <i.zobenko@ecomilk.ru>
2019-08-12 23:15:17 +0300

    File screen item added

---
+++ src/ScreenItems/File.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: he110
+ * Date: 12/08/2019
+ * Time: 22:40
+ */
+
+namespace He110\CommunicationTools\ScreenItems;
+
+
+class File implements ScreenItemInterface
+{
+    const FILE_TYPE_IMAGE = "image";
+    const FILE_TYPE_DOCUMENT = "document";
+
+    /** @var string|null */
+    private $path;
+
+    /** @var string|null */
+    private $name;
+
+    /** @var string|null */
+    private $type;
+
+    /** @var int|null */
+    private $size;
+
+    /**
+     * @return string|null
+     */
+    public function getPath(): ?string
+    {
+        return $this->path ?? null;
+    }
+
+    /**
+     * @param string $path
+     * @return File
+     */
+    public function setPath(string $path): self
+    {
+        if (file_exists($path)) {
+            $this->setSize(filesize($path));
+            if (is_null($this->getName()))
+                $this->setName(basename($path));
+            if (is_null($this->getType()))
+                $this->setType($this->isImage($path) ? static::FILE_TYPE_IMAGE : static::FILE_TYPE_DOCUMENT);
+        }
+        $this->path = $path;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name ?? null;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): ?string
+    {
+        return $this->type ?? null;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSize(): int
+    {
+        return $this->size ?? 0;
+    }
+
+    /**
+     * @param int $size
+     */
+    private function setSize(int $size): void
+    {
+        $this->size = $size;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toArray(): array
+    {
+        return array(
+            "path" => $this->getPath(),
+            "name" => $this->getName(),
+            "size" => $this->getSize(),
+            "type" => $this->getType()
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fromArray(array $data)
+    {
+        if (isset($data["path"]))
+            $this->setPath($data["path"]);
+        foreach($data as $key=>$value) {
+            $methodName = "set".ucfirst($key);
+            if (method_exists($this, $methodName) && is_callable([$this, $methodName]))
+                $this->{$methodName}($value);
+        }
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    static public function create(array $config)
+    {
+        return (new File())->fromArray($config);
+    }
+
+    /**
+     * @return null|string
+     */
+    public function __toString(): string
+    {
+         return $this->getName();
+    }
+
+    /**
+     * @param $path
+     * @return bool
+     */
+    private function isImage($path): bool
+    {
+        $a = getimagesize($path);
+        $imageType = $a[2];
+
+        if (in_array($imageType , array(IMAGETYPE_GIF , IMAGETYPE_JPEG ,IMAGETYPE_PNG , IMAGETYPE_BMP)))
+            return true;
+        return false;
+    }
+}
---
+++ tests/ScreenItems/FileTest.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: he110
+ * Date: 12/08/2019
+ * Time: 22:56
+ */
+
+namespace He110\CommunicationToolsTests\ScreenItems;
+
+use He110\CommunicationTools\ScreenItems\File;
+use PHPUnit\Framework\TestCase;
+
+class FileTest extends TestCase
+{
+    /** @var File */
+    private $file;
+
+    /** @var string  */
+    const IMAGE_PATH = __DIR__."/../Assets/image.jpg";
+
+    /** @var string  */
+    const DOCUMENT_PATH = __DIR__."/../Assets/document.txt";
+
+    /**
+     * @covers \He110\CommunicationTools\ScreenItems\File::setType()
+     * @covers \He110\CommunicationTools\ScreenItems\File::setPath()
+     * @covers \He110\CommunicationTools\ScreenItems\File::setName()
+     * @covers \He110\CommunicationTools\ScreenItems\File::setSize()
+     * @covers \He110\CommunicationTools\ScreenItems\File::getPath()
+     * @covers \He110\CommunicationTools\ScreenItems\File::getType()
+     * @covers \He110\CommunicationTools\ScreenItems\File::isImage()
+     */
+    public function testSetPath()
+    {
+        $this->file->setPath(static::IMAGE_PATH);
+        $this->assertEquals(static::IMAGE_PATH, $this->file->getPath());
+        $this->assertEquals("image.jpg", $this->file->getName());
+        $this->assertEquals(File::FILE_TYPE_IMAGE, $this->file->getType());
+    }
+
+    /**
+     * @covers \He110\CommunicationTools\ScreenItems\File::fromArray()
+     * @covers \He110\CommunicationTools\ScreenItems\File::toArray()
+     */
+    public function testToArray()
+    {
+        $conf = [
+            "path" => static::IMAGE_PATH,
+            "name" => __METHOD__
+        ];
+        $this->file->fromArray($conf);
+        $this->assertEquals($conf["path"], $this->file->getPath());
+        $this->assertEquals($conf["name"], $this->file->getName());
+
+        $new = $this->file->toArray();
+        $this->assertArrayHasKey("path", $new);
+        $this->assertArrayHasKey("name", $new);
+        $this->assertArrayHasKey("size", $new);
+        $this->assertArrayHasKey("type", $new);
+
+        $this->assertEquals($conf["path"], $new["path"]);
+        $this->assertEquals($conf["name"], $new["name"]);
+        $this->assertEquals(File::FILE_TYPE_IMAGE, $new["type"]);
+    }
+
+    /**
+     * @covers \He110\CommunicationTools\ScreenItems\File::create()
+     */
+    public function testCreate()
+    {
+        $ob = File::create(["path" => static::IMAGE_PATH, "name" => __METHOD__]);
+        $this->assertNotEquals($this->file, $ob);
+        $this->assertEquals(File::class, get_class($ob));
+        $this->assertEquals(__METHOD__, $ob->getName());
+        $this->assertEquals(static::IMAGE_PATH, $ob->getPath());
+    }
+
+    /**
+     * @covers \He110\CommunicationTools\ScreenItems\File::__toString()
+     */
+    public function test__toString()
+    {
+        $this->file->setName(__METHOD__);
+        $this->assertEquals((string)$this->file, __METHOD__);
+    }
+
+    /**
+     * @dataProvider getSizeProvider
+     *
+     * @covers \He110\CommunicationTools\ScreenItems\File::getSize()
+     * @covers \He110\CommunicationTools\ScreenItems\File::setPath()
+     * @covers \He110\CommunicationTools\ScreenItems\File::setSize()
+     * @covers \He110\CommunicationTools\ScreenItems\File::isImage()
+     */
+    public function testGetSize(string $file, int $size, string $type)
+    {
+        $this->file->setPath($file);
+        $this->assertEquals($size, $this->file->getSize());
+        $this->assertEquals($type, $this->file->getType());
+    }
+
+    /**
+     * @return array
+     */
+    public function getSizeProvider(): array
+    {
+        return array(
+            array(static::IMAGE_PATH, 39481, File::FILE_TYPE_IMAGE),
+            array(static::DOCUMENT_PATH, 12, File::FILE_TYPE_DOCUMENT)
+        );
+    }
+
+    /**
+     * @covers \He110\CommunicationTools\ScreenItems\File::getName()
+     * @covers \He110\CommunicationTools\ScreenItems\File::setName()
+     */
+    public function testSetName()
+    {
+        $this->assertNull($this->file->getName());
+        $this->file->setName(__METHOD__);
+        $this->assertEquals(__METHOD__, $this->file->getName());
+    }
+
+    public function testFromArray()
+    {
+
+    }
+
+    /**
+     * @covers \He110\CommunicationTools\ScreenItems\File::getType()
+     * @covers \He110\CommunicationTools\ScreenItems\File::setType()
+     */
+    public function testSetType()
+    {
+        $this->assertNull($this->file->getType());
+        $this->file->setType(File::FILE_TYPE_DOCUMENT);
+        $this->assertEquals(File::FILE_TYPE_DOCUMENT, $this->file->getType());
+    }
+
+    public function setUp(): void
+    {
+        $this->file = new File();
+    }
+
+    public function tearDown(): void
+    {
+        $this->file = null;
+        unset($this->file);
+    }
+}
